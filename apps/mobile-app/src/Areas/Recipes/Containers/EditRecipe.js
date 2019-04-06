1fc46aa8b582154bdff0d0dca8559cc0267c27e3
Allan Christian Carlos <allanchristiancarlos@gmail.com>
2019-04-06 17:23:03 +0800

    feat(edit-recipe): add image picker input

--- apps/mobile-app/src/Areas/Recipes/Containers/EditRecipe.js
+++ apps/mobile-app/src/Areas/Recipes/Containers/EditRecipe.js
@@ -3,7 +3,15 @@ import { View, ScrollView } from 'react-native';
 import Layout from '../../../Components/Layout';
 import { Section } from '../../../Components/Section';
 import ToggleButton from '../../../Components/ToggleButton';
-import { Text, TextBox, Button } from '../../../Core';
+import { Text, TextBox, Button, Image, TappableText } from '../../../Core';
+import { ImagePicker, Permissions } from 'expo';
+
+const IMAGE_PICKER_OPTIONS = {
+  mediaTypes: ImagePicker.MediaTypeOptions.Images,
+  allowsEditing: false,
+  base64: true,
+  aspect: [4, 3]
+};
 
 class EditRecipeContainer extends Component {
   static navigationOptions = () => {
@@ -12,11 +20,14 @@ class EditRecipeContainer extends Component {
     };
   };
 
+  static noImageUrl = 'https://placehold.it/100x100?text=no%20image';
+
   constructor(props) {
     super(props);
 
     this.state = {
-      difficulty: 'Easy'
+      difficulty: 'Easy',
+      imageUrl: EditRecipeContainer.noImageUrl
     };
   }
 
@@ -38,6 +49,35 @@ class EditRecipeContainer extends Component {
     }));
   };
 
+  _handlePressChoosePhoto = () => {
+    Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL)
+      .then(() => ImagePicker.launchImageLibraryAsync(IMAGE_PICKER_OPTIONS))
+      .then(({ cancelled, base64 }) => {
+        if (cancelled) {
+          return;
+        }
+        this._setImage(base64);
+      });
+  };
+
+  _setImage(base64Image) {
+    this.setState(state => ({
+      ...state,
+      imageUrl: `data:image/jpeg;base64,${base64Image}`
+    }));
+  }
+
+  _handlePressTakePhoto = () => {
+    Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL)
+      .then(() => ImagePicker.launchCameraAsync(IMAGE_PICKER_OPTIONS))
+      .then(({ cancelled, base64 }) => {
+        if (cancelled) {
+          return;
+        }
+        this._setImage(base64);
+      });
+  };
+
   _handleAddInstruction = () => {
     this.props.navigation.navigate('RecipeTextModal', {
       title: 'Add Instruction',
@@ -52,7 +92,7 @@ class EditRecipeContainer extends Component {
 
   render() {
     const difficulties = ['Easy', 'Moderate', 'Difficult'];
-    const { difficulty } = this.state;
+    const { difficulty, imageUrl } = this.state;
     return (
       <Layout>
         <View style={{ flexDirection: 'column', flex: 1 }}>
@@ -71,6 +111,42 @@ class EditRecipeContainer extends Component {
                   <View style={{ marginBottom: 20 }}>
                     <View style={{ marginBottom: 10 }}>
                       <Text size="small" bold letterCase="upper">
+                        Photo
+                      </Text>
+                    </View>
+                    <View
+                      style={{
+                        flex: 1,
+                        flexDirection: 'row',
+                        alignItems: 'center'
+                      }}
+                    >
+                      <View style={{ marginRight: 10 }}>
+                        <Image size="thumbnail" url={imageUrl} />
+                      </View>
+                      <View style={{ flex: 1 }}>
+                        <Button
+                          onPress={this._handlePressChoosePhoto}
+                          size="small"
+                          look="bare"
+                          kind="primary"
+                        >
+                          Choose a Photo
+                        </Button>
+                        <Button
+                          onPress={this._handlePressTakePhoto}
+                          size="small"
+                          look="bare"
+                          kind="primary"
+                        >
+                          Take a Photo
+                        </Button>
+                      </View>
+                    </View>
+                  </View>
+                  <View style={{ marginBottom: 20 }}>
+                    <View style={{ marginBottom: 10 }}>
+                      <Text size="small" bold letterCase="upper">
                         Difficulty
                       </Text>
                     </View>
@@ -78,9 +154,7 @@ class EditRecipeContainer extends Component {
                       {difficulties.map((item, index) => (
                         <View key={index} style={{ flex: 1 }}>
                           <ToggleButton
-                            onPress={() =>
-                              this._handleChangeDifficulty(item)
-                            }
+                            onPress={() => this._handleChangeDifficulty(item)}
                             block
                             active={item === difficulty}
                           >
